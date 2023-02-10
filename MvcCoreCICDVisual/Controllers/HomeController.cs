9c82b01acbb516b0b97a0f473ad7d79a38b5fa2a
serraguti <46522403+serraguti@users.noreply.github.com>
2023-02-10 19:43:57 +0100

    commit HomeController

--- MvcCoreCICDVisual/Controllers/HomeController.cs
+++ MvcCoreCICDVisual/Controllers/HomeController.cs
@@ -15,10 +15,10 @@ namespace MvcCoreCICDVisual.Controllers
 
         public IActionResult Index()
         {
-            ViewData["SALUDO"] = "Bienvenido a mi App!!!";
+            ViewData["SALUDO"] = "Mi App desde GITHUB!!!";
             Random random = new Random();
             List<int> numeros = new List<int>();
-            for (int i = 1; i <= 10; i++)
+            for (int i = 1; i <= 100; i++)
             {
                 int aleat = random.Next(1, 100);
                 numeros.Add(aleat);
