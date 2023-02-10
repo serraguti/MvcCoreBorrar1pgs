f442b249d5f197c697cbf16682ac18c05ba69ed6
serraguti <pacoserranox@gmail.com>
2023-02-10 19:30:59 +0100

    Add project files.

---
+++ MvcCoreCICDVisual/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace MvcCoreCICDVisual.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
